32050ad5ecd0c3c2c11d352fb86792f3d39c29fc
tabachukluda <121580942+tabachukluda@users.noreply.github.com>
2023-08-05 19:30:30 +0200

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
-export const App = () => {
-  return (
+import React, { useState } from 'react';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
+
+export default function App (){
+  const [feedbacks, setFeedbacks] = useState({
+    Good: 0,
+    Neutral: 0,
+    Bad: 0,
+  });
+
+  const handleFeedback = (feedbackType) => {
+    setFeedbacks((prevFeedbacks) => ({
+      ...prevFeedbacks,
+      [feedbackType]: prevFeedbacks[feedbackType] + 1,
+    }));
+  };
+
+  const totalFeedback = () => {
+    return feedbacks.Good + feedbacks.Neutral + feedbacks.Bad;
+  };
+
+  const positivePercentage = () => {
+    const total = totalFeedback();
+    const { Good } = feedbacks;
+    if (total === 0) {
+      return 0;
+    }
+    return Math.round((Good / total) * 100);
+  };
+
+  const totalFeedbackCount = totalFeedback();
+
+    return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['Good', 'Neutral', 'Bad']}
+          onLeaveFeedback={handleFeedback}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        {totalFeedbackCount !== 0 ? (
+          <Statistics
+            good={feedbacks.Good}
+            neutral={feedbacks.Neutral}
+            bad={feedbacks.Bad}
+            total={totalFeedbackCount}
+            positivePercentage={positivePercentage()}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
     </div>
   );
-};
+}
---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,9 @@
+import propTypes from 'prop-types';
+import css from './Notification.module.css'
+
+export const Notification = ({ message }) => <p className={css.message}>{message}</p>;
+
+
+Notification.propTypes = {
+    message: propTypes.string.isRequired,
+};
